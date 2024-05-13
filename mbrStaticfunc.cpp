0bcf88f9c629c6197c1be1cd8baedf8b8c2a6746
isro24 <alamat_email_anda@example.com>
2024-05-13 15:06:59 +0700

    membaut static void untuk menentukan nim dan mendapatkan nim dan menentukan id untuk mahasiswa

--- mbrStaticfunc.cpp
+++ mbrStaticfunc.cpp
@@ -13,4 +13,11 @@ class Mahasiswa
         void setID();
         void printAll();
 
+        static void setNim(int pNim) { nim = pNim;}
+        static int getNim() { return nim; }
+
+        Mahasiswa(string pnama):nama(pnama)
+        {
+            setID();
+        }
 };
