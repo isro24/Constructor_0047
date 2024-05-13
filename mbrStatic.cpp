af0aa25cbf3dd5041d4d3967dd047142db04fd86
isro24 <alamat_email_anda@example.com>
2024-05-13 14:53:07 +0700

    menampilkan output mahasiswa beserta id dari printall

--- mbrStatic.cpp
+++ mbrStatic.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa{
+class Mahasiswa{
     public:
         static int nim;
         int id;
@@ -10,22 +10,35 @@ class mahasiswa{
         void setID();
 
         void printAll();
-        mahasiswa(string pnama):nama(pnama)
+        Mahasiswa(string pnama):nama(pnama)
         {
             setID();
         }
 };
 
-int mahasiswa::nim = 20;
+int Mahasiswa::nim = 20;
 
-void mahasiswa::setID()
+void Mahasiswa::setID()
 {
     id = nim++;
 }
 
-void mahasiswa::printAll()
+void Mahasiswa::printAll()
 {
     cout << "ID  =" << id << endl;
     cout << "Nama=" << nama << endl;
     cout << endl;
+}
+
+int main(){
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+    return 0;
 }
