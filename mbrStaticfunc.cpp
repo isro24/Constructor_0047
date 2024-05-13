c2c7072063aed9f585d2fbe60e22f46d5c4d21af
isro24 <alamat_email_anda@example.com>
2024-05-13 15:10:53 +0700

    menentukan nim 100 dan membuat output nama dan id dengan nim diatas 100

--- mbrStaticfunc.cpp
+++ mbrStaticfunc.cpp
@@ -21,3 +21,18 @@ class Mahasiswa
             setID();
         }
 };
+
+
+int Mahasiswa::nim = 100;
+
+void Mahasiswa::setID()
+{
+    id = ++nim;
+}
+
+void Mahasiswa::printAll()
+{
+    cout << "ID   = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
